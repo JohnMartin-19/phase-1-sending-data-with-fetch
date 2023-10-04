79b7ddf14e96ad388df9fb706dc745f2ecf7eded
John <johnnkonge2020@gmail.com>
2023-10-04 16:13:09 +0300

    tadaaa

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Add your code here
+const userData = {
+        name:'John',
+        email:'john@gmail.com',
+    };
+
+const submitData = {
+    method:'POST',
+    headers: {
+        'Content-Type':'application/json',
+        Accept:'application/json'
+    },
+    body:JSON.stringify(userData)
+};
+fetch("http://localhost:3000/users", submitData)
+.then (function(response){
+    return response.json()
+})
+.then (function(object){
+    console.log(object)
+})
+.catch(function(error){
+    alert('ERror 404.Bad Request')
+});
+
